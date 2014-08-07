7af02dfe3c3c3beb1448f5ab3cfcd41b039f15dc
Anantya Wibowo <anantyaw@gmail.com>
2014-08-08 02:02:28 +0700

    invoke authenticated rest

--- middleware/src/main/java/com/middleware/RegistrationController.java
+++ middleware/src/main/java/com/middleware/RegistrationController.java
@@ -1,14 +1,20 @@
 package com.middleware;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class RegistrationController {
@@ -25,5 +31,34 @@ public class RegistrationController {
                 String.format(template, name));
     	
     	System.out.println(registration);
+    	
+    	//panggil surveyor minta question
+        
+        StringBuilder sb = new StringBuilder();
+		try {
+		    URL url = new URL("http://localhost:8383/getQuestion");
+		    URLConnection urlConnection = setUsernamePassword(url);
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        sb.append(line);
+		    }
+		    reader.close();
+		
+		    System.out.println("ini dari rest sebelah");
+		    System.out.println(sb.toString()); // parse json ini ke object
+		    System.out.println("-------------------------------------------");
+		} catch (Exception e) {
+		    throw new RuntimeException(e);
+		}
+       
+    }
+
+    private URLConnection setUsernamePassword(URL url) throws IOException {
+        URLConnection urlConnection = url.openConnection();
+        String authString = "user" + ":" + "password";
+        String authStringEnc = new String(Base64.encodeBase64(authString.getBytes()));
+        urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+        return urlConnection;
     }
 }
